fe5a76931604ad32547597fd42e413adfad3e347
REDpapa <akamine.sigoto@gmail.com>
2021-06-22 06:48:37 +0900

    🔴django説明中

--- django.py
+++ django.py
@@ -55,28 +55,77 @@ index_dict['requirements.txtを作成してライブラリーを管理する。'
 ![](https://user-images.githubusercontent.com/79512367/122568244-85900200-d084-11eb-93a2-87f840e9dba0.png)
 
 #### requirements.txt に記載されたライブラリーを元にライブラリーをinstallする。
+```
+pip install -r requirements.txt
+```
 ![](https://user-images.githubusercontent.com/79512367/122568921-36969c80-d085-11eb-8815-3eccc6cf83d8.png)
     """
 )
 
-# リスト内の値を結合する。(要素に数値型が含まれる場合)
-index_dict['リスト内の値を結合する。(要素に数値型が含まれる場合)'] = (
+# プロジェクトの作成(django-admin startproject mysite .)
+index_dict['プロジェクトの作成(django-admin startproject mysite .)'] = (
     """
-```python
-# リスト内の値を結合する。(要素に数値型が含まれる場合)
-list = ['A', 'B', 'C', 1, 2, 3]
-
-map = map(str, list)
-
-print(''.join(map))
+#### プロジェクトの作成
 ```
-#### 出力結果
-```bash
-ABC123
+django-admin startproject mysite .
 ```
+
+![](https://user-images.githubusercontent.com/79512367/122824259-6363e180-d31b-11eb-9efd-9ba512c80d01.png)
+
+#### 作業ディレクトリに『mysite』ディレクトリが作成される
+
+![](https://user-images.githubusercontent.com/79512367/122824991-4e3b8280-d31c-11eb-8307-2ab502578334.png)
     """
 )
 
+# settings.py にて、言語設定と時間設定を変更する
+index_dict['settings.py にて、言語設定と時間設定を変更する'] = (
+    """
+#### mysite -> settings.py を開く \nLANGUAGE_CODE と TIME_ZONE の設定を変更する
+
+![](https://user-images.githubusercontent.com/79512367/122827105-edfa1000-d31e-11eb-8c41-c96cfb1233b9.png)
+
+![](https://user-images.githubusercontent.com/79512367/122827110-ef2b3d00-d31e-11eb-8608-50dbcf7d3282.png)
+
+#### 日本語で東京の時間に設定したことです。
+    """
+
+)
+
+# 画像をアップロードするファイルを指定
+index_dict['画像をアップロードするファイルを指定'] = (
+    """
+#### mysite -> settings.py 末端にコードを追加する。
+
+```python
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+```
+![](https://user-images.githubusercontent.com/79512367/122829695-4848a000-d322-11eb-97b7-7e20eaf524ea.png)
+
+#### os ライブラリーのimprotができていないので追加
+
+```python
+import os
+```
+![](https://user-images.githubusercontent.com/79512367/122830624-8f836080-d323-11eb-9983-b4d5791fdf2a.png)    """
+
+)
+
+# データベースを構築する(migrate)
+index_dict['データベースを構築する(migrate)'] = (
+    """
+#### ターミナルにて『 python manage.py migrate 』を実行する
+
+```
+python manage.py migrate
+```
+![](https://user-images.githubusercontent.com/79512367/122831208-7fb84c00-d324-11eb-9323-030602b276df.png)
+
+#### 作業ディレクトリに db.sqlite3 のデータベースが作成される。
+![](https://user-images.githubusercontent.com/79512367/122831808-6368df00-d325-11eb-89c1-c871eb088bda.png)
+    """
+)
 # ************************************************************
 
 index_keys_list = list(index_dict.keys())
