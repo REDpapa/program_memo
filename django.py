4024b2fc992f99b78fbf7157697b2f69642efea6
REDpapa <akamine.sigoto@gmail.com>
2021-06-25 16:18:07 +0900

    🔴djangoの部分を追加

--- django.py
+++ django.py
@@ -39,7 +39,7 @@ source myvenv/bin/activate
 ```
 (myvenv) ◆自分のパソコンのpath◆ django_myportfolio %
 ```
-##### 再起動などで再度始める際は、アクティベイト操作をして仮想環境に入ろう!
+### 再起動などで再度始める際は、アクティベイト操作をして仮想環境に入ろう!
     """
 
 
@@ -124,6 +124,165 @@ python manage.py migrate
 
 #### 作業ディレクトリに db.sqlite3 のデータベースが作成される。
 ![](https://user-images.githubusercontent.com/79512367/122831808-6368df00-d325-11eb-89c1-c871eb088bda.png)
+    """
+)
+
+# runserverを起動してみよう。
+index_dict['runserverを起動してみよう。'] = (
+    """
+#### ターミナルにて『 python manage.py runserver 』を実行する。
+#### 囲いの中のURLに『 command + クリック 』でアクセスする。
+
+```
+python manage.py runserver
+```
+![](https://user-images.githubusercontent.com/79512367/123351381-f27d2d80-d597-11eb-92b7-787146ec0b57.png)
+
+
+#### アクセス先がこのような画面になれば、問題ありません!!
+![](https://user-images.githubusercontent.com/79512367/123351763-d62dc080-d598-11eb-9483-f7953e5bb376.png)
+    """
+)
+
+# runserverを終了してみよう。
+index_dict['runserverを終了してみよう。(control + c)'] = (
+    """
+#### ターミナルの画面にて『 control + c 』を押す。これだけ!
+
+![](https://user-images.githubusercontent.com/79512367/123352138-a337fc80-d599-11eb-90dd-72478487da93.png)
+
+#### ※ runserverを終了させたので、URLのアクセス先がアクセスできなくなります。
+
+![](https://user-images.githubusercontent.com/79512367/123352462-49840200-d59a-11eb-9091-77d7e50e2113.png)
+    """
+)
+
+# アプリケーションを作成してみよう。(python manage.py startapp app)
+index_dict['アプリケーションを作成してみよう。(python manage.py startapp app)'] = (
+    """
+#### ターミナルの画面にて『 python manage.py startapp app 』を実行。
+作業フォルダーに、app(アプリケーション)をいうフォルダーが追加される。
+
+```
+python manage.py startapp app
+```
+
+![](https://user-images.githubusercontent.com/79512367/123356975-b5b73380-d5a3-11eb-8bcf-db8273821ab0.png)
+
+    """
+)
+
+# アプリケーションを使えるよに設定を変更する。(settings.py の INSTALLED_APPSに追加)
+index_dict['アプリケーションを使えるよに設定を変更する。(settings.py の INSTALLED_APPSに追加)'] = (
+    """
+#### settings.pyファイルの 環境変数 INSTALLED_APPS にアプリを追加する作業をする。
+1. app 作成したアプリケーションを追加
+2. widget_tweaks フォーム画面を作る時に便利なので追加
+
+```python
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'widget_tweaks',  # ここの部分を追加
+    'app',  # ここの部分を追加
+]
+```
+
+![](https://user-images.githubusercontent.com/79512367/123370199-33863980-d5ba-11eb-9a17-62aef6be18bc.png)
+
+    """
+)
+
+# モデルを作成していく。(app -> models.py)
+index_dict['モデルを作成していく。(app -> models.py)'] = (
+    """
+#### appフォルダのmodels.pyファイルを開き作業していく。
+1. appフォルダ -> models.pyファイル を開く。
+2. Profile(プロフィール)のモデルクラスを作成する。
+
+作業ファイルはここ!!
+![](https://user-images.githubusercontent.com/79512367/123371775-52d29600-d5bd-11eb-9232-249dedda2643.png)
+
+
+#### app -> models.py にプロフィール用のコードを入力。
+
+
+```python
+class Profile(models.Model):
+    title = models.CharField('タイトル', max_length=100, null=True, blank=True)
+    subtitle = models.CharField('サブタイトル', max_length=100, null=True, blank=True)
+    name = models.CharField('名前', max_length=100)
+    job = models.TextField('仕事')
+    introduction = models.TextField('自己紹介')
+    github = models.CharField('github', max_length=100, null=True, blank=True)
+    twitter = models.CharField('twitter', max_length=100, null=True, blank=True)
+    linkedin = models.CharField('linkedin', max_length=100, null=True, blank=True)
+    facebook = models.CharField('facebook', max_length=100, null=True, blank=True)
+    instagram = models.CharField('instagram', max_length=100, null=True, blank=True)
+    topimage = models.ImageField(upload_to='images', verbose_name='トップ画像')
+    subimage = models.ImageField(upload_to='images', verbose_name='サブ画像')
+
+    def __str__(self):
+        return self.name
+```
+
+![](https://user-images.githubusercontent.com/79512367/123373534-7ea34b00-d5c0-11eb-9c78-2590cb246dcd.png)
+
+    """
+)
+
+# 管理画面の作成を行う。(app -> admin.py)
+index_dict['管理画面の作成を行う。(app -> admin.py)'] = (
+    """
+
+## 管理画面を作成して、データを登録できるようにする。
+
+#### appフォルダのadmin.pyファイルを開き作業していく。
+1. appフォルダ -> admin.pyファイル を開く。
+2. models.py で作成した Profile クラスを呼び出してくるコードを作成。
+
+
+#### 作業ファイルはここ!!
+
+![](https://user-images.githubusercontent.com/79512367/123382855-64239e80-d5cd-11eb-8f01-d2e333b33092.png)
+
+#### models.py で作成した Profile クラスを呼び出してくるコードを作成。
+追加するコードは、これ!!
+#### admin の管理画面のモデルをProfileで作成しているイメージ
+
+```python
+from django.contrib import admin
+from .models import Profile  # これを追加
+
+# Register your models here.
+admin.site.register(Profile)  # これを追加
+```
+![](https://user-images.githubusercontent.com/79512367/123383672-6a664a80-d5ce-11eb-9a38-21727a3a9aa0.png)
+
+    """
+)
+
+# データベースを再構築する。(python manage.py makemigrations)
+index_dict['データベースを再構築する。(python manage.py makemigrations)'] = (
+    """
+
+## モデルを追加したので、データベースの再構築をする。
+
+#### ターミナルにて『 python manage.py makemigrations 』を実行する。
+
+
+```
+python manage.py makemigrations
+```
+
+#### こんな感じになれば OK!!
+
+![](https://user-images.githubusercontent.com/79512367/123385516-95ea3480-d5d0-11eb-9ef0-1e6f053d4877.png)
+
     """
 )
 # ************************************************************
